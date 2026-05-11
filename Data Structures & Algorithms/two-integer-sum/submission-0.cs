e6b9314cf9dc4e425b08f6df9c6925776ce5e6f7
Cai Quang Tùng | TungDira <126932152+caiquangtung@users.noreply.github.com>
2026-05-11 23:59:35 +0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> seen = new Dictionary<int , int>();
+        for (int i = 0; i< nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if(seen.ContainsKey(complement))
+            {
+                return new int[]{seen[complement],i};
+            }
+            if(!seen.ContainsKey(complement))
+            {
+                seen[nums[i]] = i;
+            }
+        }
+        return new int[] { };
+    }
+}
